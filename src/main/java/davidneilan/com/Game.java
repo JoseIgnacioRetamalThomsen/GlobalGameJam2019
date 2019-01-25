cadbc8a876aeb805421b7d8fad9404a1bc87308e
Ronan <g00342393@gmit.ie>
2019-01-25 21:35:16 +0000

    Actually added scaling

--- src/main/java/davidneilan/com/Game.java
+++ src/main/java/davidneilan/com/Game.java
@@ -1,68 +1,46 @@
 package davidneilan.com;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+
+import java.awt.*;
 
 /**
  * A game using Slick2d
  */
 public class Game extends BasicGame {
-
     /** Screen width */
-    private static final int WIDTH = 800;
+    private static final int WIDTH = 1920;
     /** Screen height */
-    private static final int HEIGHT = 600;
-    
-    /** A counter... */
-    private double counter;
+    private static final int HEIGHT = 1080;
 
-    private Explosion1 ex;
+    private Image img;
 
     public Game() {
         super("A Slick2d game");
     }
 
-
-
-    public void render(GameContainer container, Graphics g) throws SlickException {
-        g.setColor(Color.white);
-        g.drawString("Hello, " + Double.toString(counter) + "!", 50, 50);
-
-        // rotating square
-        int centerX = WIDTH / 2;
-        int centerY = HEIGHT / 2;
-        int squareWidth = 50;
-        int rotateDist = 200;
-        double stage = ((counter % 1000.0) / 1000) * (2 * Math.PI);
-
-        g.setColor(Color.white);
-
-        int squareX = centerX + (int) (Math.cos(stage) * rotateDist) - (squareWidth / 2);
-        int squareY = centerY + (int) (Math.sin(stage) * rotateDist) - (squareWidth / 2);
-
-        g.fillRect(squareX - (squareWidth / 2), squareY - (squareWidth / 2), squareWidth, squareWidth);
-
-
-        //draw test animation
-        ex.draw();
-
-    }
-
     @Override
     public void init(GameContainer container) throws SlickException {
-        counter = 0;
-
-        ex = new Explosion1();
 
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
-        counter += delta;
+
+    }
+
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        img.draw(20, 20);
     }
     
     public static void main(String[] args) throws SlickException {
-        AppGameContainer app = new AppGameContainer(new Game());
-        app.setDisplayMode(WIDTH, HEIGHT, false);
+        AppGameContainer app = new AppGameContainer(new ScalableGame(new Game(), WIDTH, HEIGHT, true));
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        app.setDisplayMode(screenSize.width, screenSize.height, true);
         app.setForceExit(false);
         app.setTargetFrameRate(60);
         app.setMaximumLogicUpdateInterval(16);
