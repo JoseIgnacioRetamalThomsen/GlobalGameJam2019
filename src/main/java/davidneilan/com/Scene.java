6162869ad9c0b06dd7b91b3436de2a40e463da56
Ronan <g00342393@gmit.ie>
2019-01-26 00:29:32 +0000

    Scene additions

--- src/main/java/davidneilan/com/Scene.java
+++ src/main/java/davidneilan/com/Scene.java
@@ -2,14 +2,29 @@ package davidneilan.com;
 
 import org.newdawn.slick.Image;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.List;
 
 public class Scene {
     private Image background;
     private BufferedImage objectMap;
+    private List<SceneObject> sceneObjects;
 
-    public Scene(Image background, BufferedImage objectMap) {
+    public Scene(Image background, BufferedImage objectMap, List<SceneObject> sceneObjects) {
         this.background = background;
         this.objectMap = objectMap;
+        this.sceneObjects = sceneObjects;
+    }
+
+    public void onSceneClick(int clickX, int clickY) {
+        Color clickedColor = new Color(objectMap.getRGB(clickX, clickY));
+
+        for (SceneObject obj : sceneObjects) {
+            if (obj.getColor().equals(clickedColor)) {
+                obj.onClick();
+                return;
+            }
+        }
     }
 }
--- src/main/java/davidneilan/com/SceneObject.java
+++ src/main/java/davidneilan/com/SceneObject.java
@@ -3,18 +3,24 @@ package davidneilan.com;
 import org.newdawn.slick.Animation;
 
 import java.awt.Color;
+import java.util.List;
 
 public class SceneObject {
     private Color color;
     private Animation anim;
+    private List<Item> items;
 
     public SceneObject(Color color, Animation anim) {
         this.color = color;
         this.anim = anim;
     }
 
-    public boolean clickInBounds(int clickX, int clickY) {
-        // TODO
-        return false;
+    public Color getColor() {
+        return color;
     }
+
+    public void onClick() {
+
+    }
+
 }
