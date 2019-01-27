a3070a9bbd6853a81d9f8bc38ef69bdeca169242
pepe <peperetamalthomsen@outlook.com>
2019-01-27 10:05:32 +0000

    some scenes

--- src/main/java/davidneilan/com/SceneManager.java
+++ src/main/java/davidneilan/com/SceneManager.java
@@ -1,5 +1,6 @@
 package davidneilan.com;
 
+import davidneilan.com.game_state.PlayingGameState;
 import org.newdawn.slick.*;
 
 import javax.imageio.ImageIO;
@@ -10,10 +11,10 @@ import java.util.List;
 
 public class SceneManager {
     private Scene[] scenes;
-    private int currentScene;
+    public static int currentScene;
 
     public void init() throws SlickException {
-        scenes = new Scene[2];
+        scenes = new Scene[5];
         currentScene = 0;
 
         List<SceneObject> sceneObjs;
@@ -39,8 +40,8 @@ public class SceneManager {
 
         sceneObjInv = new SceneObject("Weapon Placer", Color.red, blankAnim);
 
-        sceneObjInv.addTakableItem(new Item("Weapon", new Image("Assets/Sprites/key.png")),
-                new ItemScreenPosition(1200, 250, 30));
+        sceneObjInv.addTakableItem(new Item("Weapon", new Image("Assets/Sprites/tyreiron.png")),
+                new ItemScreenPosition(1000, 550, 35));
 
         sceneObj.setItemShowingFrame(1);
         sceneObjs.add(sceneObj);
@@ -53,15 +54,47 @@ public class SceneManager {
         objMap = new Image("Assets/Sprites/scene1-object-map.png");
         scenes[0] = new Scene(sceneBg, objMap, sceneObjs);
 
-        // init scene 2
+        // init scene 2 street
         sceneBg = new Image("Assets/Backgrounds/Street1.png");
         objMap = new Image("Assets/Sprites/street-object-map.png");
         sceneObjs = new ArrayList<>();
         anim = new Animation();
         anim.addFrame(new Image("Assets/Sprites/scene-1-door.png"), 1);
-        sceneObj = new SceneObject("pawn door", Color.red, anim);
-        sceneObjs.add(sceneObj);
+        sceneObjs.add(new SceneObject("pawn door",new Color(255, 0, 0), anim));
+        sceneObjs.add( new SceneObject("metro", new Color(0, 255, 0), anim));
+       // sceneObjs.add(sceneObj);
         scenes[1] = new Scene(sceneBg, objMap, sceneObjs);
+
+        //init scene 3 bad guys after door
+        sceneBg = new Image("Assets/Backgrounds/badguys.png");
+        objMap = new Image("Assets/Sprites/badguysMap.png");
+
+        sceneObjs = new ArrayList<>();
+        sceneObjs.add(new SceneObject("go", new Color(255, 0, 0), blankAnim));
+        sceneObjs.add(new SceneObject("talk", new Color(255, 216, 0), blankAnim));
+       // sceneObjs.add(sceneObj);
+        scenes[2] = new Scene(sceneBg, objMap, sceneObjs);
+
+        //init scene 4 pawn show
+        sceneBg = new Image("Assets/Backgrounds/pawnshop.png");
+        objMap = new Image("Assets/Sprites/pawnshop_map.png");
+        sceneObjs = new ArrayList<>();
+        sceneObjs.add(new SceneObject("back", new Color(72, 0, 255), blankAnim));
+        sceneObjs.add(new SceneObject("shop", new Color(255, 0, 0), blankAnim));
+       // sceneObjs.add(sceneObj);
+        scenes[3] = new Scene(sceneBg, objMap, sceneObjs);
+
+        //init scene 5 metro
+        sceneBg = new Image("Assets/Backgrounds/metro.png");
+        objMap = new Image("Assets/Sprites/metromap.png");
+
+        sceneObjs.add(new SceneObject("back", new Color(0, 38, 255), blankAnim));
+        sceneObjs.add(new SceneObject("shop", new Color(255, 0, 110), blankAnim));
+        sceneObjs.add(new SceneObject("metro", new Color(255, 0, 0), blankAnim));
+        sceneObjs.add(new SceneObject("talk", new Color(0, 255, 33), blankAnim));
+       // sceneObjs.add(sceneObj);
+        scenes[4] = new Scene(sceneBg, objMap, sceneObjs);
+
     }
 
     public void render(Graphics g) throws SlickException {
@@ -74,5 +107,99 @@ public class SceneManager {
 
     public void goToScene(int scene) {
         currentScene = scene;
+    }
+
+    public void showDialog(String name, boolean isTextAreaOption, boolean isTextArea) {
+        switch (currentScene) {
+            case 0:
+                if (name.equals("door")) {
+
+
+                    if (PlayingGameState.inventory.hasItem("Key")) {
+                        PlayingGameState.isTextAreaOption = true;
+                        PlayingGameState.textArea.setText(String.format("%s %s %s A \n %s %s %s S",
+                                PlayingGameState.language.getString("S1_FOR"),
+                                PlayingGameState.language.getString("S6_DOOR_2"),
+                                PlayingGameState.language.getString("S1_PRESS"),
+                                PlayingGameState.language.getString("S1_FOR"),
+                                PlayingGameState.language.getString("S5_DOOR_1"),
+                                PlayingGameState.language.getString("S1_PRESS")
+                        ));
+                    } else {
+                        PlayingGameState.textArea.setText(String.format("%s", PlayingGameState.language.getString("S1_NEEDKEY")));
+                    }
+                    PlayingGameState.isTextArea = true;
+                } else if (name.equals("window")) {
+
+                    PlayingGameState.isTextAreaOption = true;
+                    PlayingGameState.textArea.setText(String.format("%s %s %s A \n %s %s %s S",
+                            PlayingGameState.language.getString("S1_FOR"),
+                            PlayingGameState.language.getString("S2_WIN_1"),
+                            PlayingGameState.language.getString("S1_PRESS"),
+                            PlayingGameState.language.getString("S1_FOR"),
+                            PlayingGameState.language.getString("S3_WIN_2"),
+                            PlayingGameState.language.getString("S1_PRESS")
+                    ));
+
+                    PlayingGameState.isTextArea = true;
+                }
+                break;
+            //street
+            case 1:
+                if (name.equals("pawn door")) {
+                    PlayingGameState.sceneManager.goToScene(3);
+                }else if(name.equals("metro")){
+                    PlayingGameState.sceneManager.goToScene(4);
+                }
+                break;
+            //bad guys
+            case 2:
+                if (name.equals("go")) {
+                    if (PlayingGameState.inventory.hasItem("Weapon")) {
+                        PlayingGameState.sceneManager.goToScene(1);
+                    }
+
+                } else if (name.equals("talk")) {
+
+                    if (PlayingGameState.inventory.hasItem("Weapon")) {
+                        PlayingGameState.textArea.setText(PlayingGameState.language.getString("S2_TALKS"));
+                        PlayingGameState.isTextArea = true;
+                    } else {
+                        PlayingGameState.textArea.setText(PlayingGameState.language.getString("S2_GAMEOVER"));
+                        PlayingGameState.isTextArea = true;
+                    }
+                }
+
+                break;
+            //shop
+            case 3:
+                if (name.equals("shop")) {
+
+                    PlayingGameState.isTextAreaOption = true;
+                    PlayingGameState.isTextArea = true;
+                    String text = String.format("%s %n",PlayingGameState.language.getString("SHOP_IN"));
+
+                    if(PlayingGameState.inventory.hasItem("Phone")){
+                        text += String.format("%s %s S %n",PlayingGameState.language.getString("SELL_PHONE"),PlayingGameState.language.getString("S1_PRESS"));
+
+                    }
+
+                    PlayingGameState.textArea.setText(text);
+
+                } else if (name.equals("back")) {
+                    PlayingGameState.sceneManager.goToScene(1);
+                }
+                break;
+            case 4:
+                if(name.equals("metro")){
+                    System.out.println("metro");
+                }else if(name.equals("back")){
+                    PlayingGameState.sceneManager.goToScene(1);
+                }else if(name.equals("shop")){
+
+                }else if(name.equals("talk")){
+
+                }
+        }
     }
 }
--- src/main/java/davidneilan/com/game_state/PlayingGameState.java
+++ src/main/java/davidneilan/com/game_state/PlayingGameState.java
@@ -23,13 +23,13 @@ public class PlayingGameState extends TransferableGameState {
     private int barX = 556, barY = 925;
 
     // private Image imgBar;
-    private Player player;
+    public static Player player;
 
-    private TextArea textArea = new TextArea();
+    public static TextArea textArea = new TextArea();
 
-    Inventory inventory;
+    public static Inventory inventory;
 
-    private SceneManager sceneManager;
+    public static SceneManager sceneManager;
 
     Item key;
     Item phone;
@@ -88,6 +88,8 @@ public class PlayingGameState extends TransferableGameState {
         tyre = new Item("Tyre", new Image("Assets/Sprites/tyre.png"));
         tyreIron = new Item("Tyre Iron", new Image("Assets/Sprites/tyreiron.png"));
 
+        inventory.addItem(phone);
+        inventory.setCach(200);
 /*
         inventory.addItem(key);
         inventory.addItem(phone);
@@ -112,8 +114,8 @@ public class PlayingGameState extends TransferableGameState {
         this.player.update();
     }
 
-    boolean isTextArea = true;
-    boolean isTextAreaOption = false;
+    public static boolean isTextArea = true;
+    public static boolean isTextAreaOption = false;
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
@@ -165,6 +167,8 @@ public class PlayingGameState extends TransferableGameState {
     int x1, y1;
     int clickedBox;
 
+    public static String action = "";
+
     @Override
     public void mousePressed(int button, int x, int y) {
         x1 = x;
@@ -186,41 +190,9 @@ public class PlayingGameState extends TransferableGameState {
         ClickedObjects itemClicked = sceneManager.onSceneClick(x, y);
 
         if (itemClicked.hasSceneObject()) {
+            action = itemClicked.getSceneObject().getName();
+            sceneManager.showDialog(action, isTextAreaOption, isTextArea);
             System.out.println("Clicked: " + itemClicked.getSceneObject().getName());
-            if (itemClicked.getSceneObject().getName().equals("door")) {
-
-                System.out.println("has key " + player.searchItem("Key"));
-                if (inventory.hasItem("Key")) {
-                    isTextAreaOption = true;
-                    this.textArea.setText(String.format("%s %s %s A \n %s %s %s S",
-                            language.getString("S1_FOR"),
-                            language.getString("S6_DOOR_2"),
-                            language.getString("S1_PRESS"),
-                            language.getString("S1_FOR"),
-                            language.getString("S6_DOOR_2"),
-                            language.getString("S5_DOOR_1")
-                    ));
-                } else {
-                    this.textArea.setText(String.format("%s", language.getString("S1_NEEDKEY")));
-                }
-                isTextArea = true;
-            }
-
-            else if( itemClicked.getSceneObject().getName().equals("window") ){
-
-                isTextAreaOption=true;
-                this.textArea.setText(String.format("%s %s %s A \n %s %s %s S",
-                        language.getString("S1_FOR"),
-                        language.getString("S2_WIN_1"),
-                        language.getString("S1_PRESS"),
-                        language.getString("S1_FOR"),
-                        language.getString("S3_WIN_2"),
-                        language.getString("S1_PRESS")
-                ));
-
-                isTextArea = true;
-            }
-
 
         } else {
             System.out.println("Clicked nothing");
@@ -254,14 +226,58 @@ public class PlayingGameState extends TransferableGameState {
         }
 
         if (isTextAreaOption) {
-            if (key == Input.KEY_A) {
-                System.out.println("Working");
-                sceneManager.goToScene(1);
-                isTextAreaOption = false;
-                isTextArea = false;
-            } else if (key == Input.KEY_S) {
-                isTextAreaOption = false;
-                isTextArea = false;
+            switch (SceneManager.currentScene) {
+                case 0:
+                    switch (action) {
+                        case "door":
+                            if (key == Input.KEY_A) {
+                                System.out.println("Working");
+                                sceneManager.goToScene(2);
+                                isTextAreaOption = false;
+                                isTextArea = false;
+                            } else if (key == Input.KEY_S) {
+                                isTextAreaOption = false;
+                                isTextArea = false;
+                            }
+                            break;
+
+                        case "window":
+                            if (key == Input.KEY_A) {
+
+                                sceneManager.goToScene(1);
+                                isTextAreaOption = false;
+                                isTextArea = false;
+                            } else if (key == Input.KEY_S) {
+                                isTextAreaOption = false;
+                                isTextArea = false;
+                            }
+                            break;
+                    }
+                    break;
+                //bad guys
+                case 2:
+                    switch (action) {
+                        case "go":
+                            sceneManager.goToScene(1);
+                            break;
+                    }
+                    break;
+
+                    //pawn shop
+                case 3:
+                    switch(action){
+                        case "shop":
+                            System.out.println("shoppppppp");
+                            if (key == Input.KEY_S) {
+                                inventory.addCash(1500);
+                                int slot = inventory.getItemSlot("Phone");
+                                System.out.println(slot);
+                                inventory.removeItem(slot+1);
+                                isTextAreaOption = false;
+                                isTextArea = false;
+                            }
+                    }
+                    break;
             }
         }
     }
