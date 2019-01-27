0c436d0780b0b2dc9382162c423a5d03a9f06028
ndipenoch <G00352031@GMIT.IE>
2019-01-27 14:24:25 +0000

    Lang change

--- src/main/java/davidneilan/com/inter/French.java
+++ src/main/java/davidneilan/com/inter/French.java
@@ -35,7 +35,7 @@ public class French implements Language {
         strings.put("NEED_TICKET","VOUS AVEZ BESOIN DE PRENDRE UN TICKET DE METRO");
 
         strings.put("METRO_1","SALUT MON PNEU EST CASSÉ J'AI BESOIN DE REPARER");
-        strings.put("METRO_2","OOH, EJ VOIR VOU AVEZ UN PNEU, POUVEZ VOUS ME DONNER? ");
+        strings.put("METRO_2","OOH, EJ VOIR VOUS AVEZ UN PNEU, POUVEZ VOUS ME DONNER? ");
         strings.put("METRO_3","POUR DONNER LE PNEU ");
         strings.put("METRO_4","MERCI, PREDRE CA... ");
     }
